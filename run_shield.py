9514e3eae161b283d238ba4e95cee5e6b6869a28
silver6wings <silver6wings@163.com>
2025-05-01 01:36:10 +0800

    Update run_shield.py

--- run_shield.py
+++ run_shield.py
@@ -3,9 +3,23 @@ from tqsdk import TqApi, TqAuth
 from credentials.tq import *
 
 
-if __name__ == '__main__':
+SYMBOL = "SHFE.au2505"
+
+
+def run(symbol: str):
     api = TqApi(auth=TqAuth(tq_username, tq_password))
-    klines = api.get_kline_serial("SHFE.au2505", 10)
+    klines = api.get_kline_serial(symbol, 10)
     print(klines)
     while api.wait_update():
+        # https://doc.shinnytech.com/tqsdk/latest/reference/tqsdk.objs.html#tqsdk.objs.Position
+        position = api.get_position(symbol=symbol)
+        print(position.open_price_long)
+        print(position.open_price_short)
+        print(position.position_cost_long)
+        print(position.position_cost_short)
+
         print("最后一根K线收盘价", klines.close.iloc[-1])
+
+
+if __name__ == '__main__':
+    run(symbol=SYMBOL)
